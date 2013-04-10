f5ed418f4673ed8e54c526ccf3dbdc89db1e8960
Jeremy MAURO <j.mauro@criteo.com>
2013-04-10 18:38:19 +0200

    First working version

--- definitions/mirror_repo.rb
+++ definitions/mirror_repo.rb
@@ -11,8 +11,8 @@
 #   description 'Repository CentOS 5.6 32 bit'
 #   release   '5.6'
 #   arch      'i386'
-#   metadata  
-#   ensure    'present'
+#   metadata  [ 'repomd' , 'yum' ]
+#   action    'create'
 #   update    'nightly'
 #   urls      {
 #     :addons      => 'rsync://mirrors.kernel.org/centos/$release/addons/$arch/',
@@ -27,13 +27,13 @@ define  :mirror_repo,
         :iso         => nil,
         :urls        => nil,
         :hour        => '0',
-        :ensure      => 'present',
+        :action      => 'create',
         :update      => 'weekly',
         :gentimeout  => '3600' do
 
   include_recipe 'mrepo'
 
-  acceptable_ensure   = [ 'present', 'absent' ]
+  acceptable_action   = [ 'create', 'delete' ]
   acceptable_metadata = [ 'yum', 'apt', 'repomd', 'repoview' ]
   acceptable_update   = [ 'now', 'weekly', 'nightly', 'never' ]
   acceptable_arch     = [ 'i386', 'i586', 'x86_64', 'ppc', 's390', 's390x', 'ia64' ]
@@ -44,8 +44,10 @@ define  :mirror_repo,
   iso            = params[:iso]
   urls           = params[:urls]
   mirror_name    = params[:name]
-  create         = params[:ensure]
+  create         = params[:action]
   update         = params[:update]
+  description    = params[:description]
+  gentimeout     = params[:gentimeout].to_i
   src_dir        = node[:mrepo][:srcdir]
   www_dir        = "#{node[:mrepo][:wwwdir]}/#{mirror_name}"
   mrepo_dir_conf = "#{node[:mrepo][:config_dir]}/#{mirror_name}.conf"
@@ -70,9 +72,9 @@ define  :mirror_repo,
   end
   # --[ Check arguments ]--
   invalide_array = {
-    :invalide_ensure => {
-      :title => 'ensure',
-      :value => [ create ] - acceptable_ensure,
+    :invalide_action => {
+      :title => 'action',
+      :value => [ create ] - acceptable_action,
     },
     :invalide_update => {
       :title => 'update',
@@ -88,14 +90,16 @@ define  :mirror_repo,
     },
   }
 
-  invalide_options = %w(invalide_ensure invalide_metadata invalide_update invalide_arch)
+  invalide_options = %w(invalide_action invalide_metadata invalide_update invalide_arch)
   invalide_options.each do |option|
     if invalide_array[:"#{option}"][:value].size == 1
-      Chef::Application.fatal! "The passed value 'invalide_array[:"#{option}"][:value]' for the option 'invalide_array[:"#{option}"][:title]' is not an acceptable value"
+      title = [:"#{option}"][:title]
+      value = [:"#{option}"][:value]
+      Chef::Application.fatal! "The passed value [#{value}] for the option [#{title}] is not an acceptable value"
     end
   end
 
-  if create == 'present'
+  if create == 'create'
 
     Chef::Log.info ">>> [:mirror_repo] Adding repo '#{mirror_name}'"
     template mrepo_dir_conf do
@@ -104,19 +108,20 @@ define  :mirror_repo,
       group 'root'
       mode  '0644'
       variables(
-        :name     => mirror_name,
-        :release  => release,
-        :arch     => arch,
-        :metadata => metadata,
-        :iso      => iso,
-        :urls     => urls,
+        :name        => mirror_name,
+        :description => description,
+        :release     => release,
+        :arch        => arch,
+        :metadata    => metadata,
+        :iso         => iso,
+        :urls        => urls,
       )
       notifies :run, "execute[Generate mrepo for #{mirror_name}]"
     end
 
-    Chef::Log.info ">>> [:mirror_repo] Adding repo '#{mirror_name}'"
+    Chef::Log.info ">>> [:mirror_repo] Generate repo '#{mirror_name}'"
     execute "Generate mrepo for #{mirror_name}" do
-      path "['/usr/bin','/bin']"
+      path ['/usr/bin','/bin']
       command "mrepo -g #{mirror_name}"
       cwd src_dir
       user 'root'
@@ -139,8 +144,8 @@ define  :mirror_repo,
       # --[ Update repo is now ]--
       Chef::Log.info ">>> [:mirror_repo] Synchronizing now repo '#{mirror_name}'"
       execute "Synchronize repo #{mirror_name}" do
-        path "['/usr/bin','/bin']"
-        command "/usr/bin/mrepo -qgu #{mirror_name}"
+        path ['/usr/bin','/bin']
+        command "/usr/bin/mrepo -gu #{mirror_name}"
         cwd src_dir
         user "root"
         group "root"
@@ -168,7 +173,7 @@ define  :mirror_repo,
         hour params[:hour]
         minute '0'
         path "/bin:/usr/bin"
-        command "/usr/bin/mrepo -qgu #{mirror_name}"
+        command "/usr/bin/mrepo -gu #{mirror_name}"
         user "root"
         home src_dir
         shell "/bin/bash"
@@ -189,7 +194,7 @@ define  :mirror_repo,
         hour params[:hour]
         minute '0'
         path "/bin:/usr/bin"
-        command "/usr/bin/mrepo -qgu #{mirror_name}"
+        command "/usr/bin/mrepo -gu #{mirror_name}"
         user "root"
         home src_dir
         shell "/bin/bash"
@@ -206,7 +211,7 @@ define  :mirror_repo,
     Chef::Log.info ">>> [:mirror_repo] Removing repo '#{mirror_name}'"
     Chef::Log.info ">>> [:mirror_repo] Umounting iso for repo '#{mirror_name}'"
     execute "Unmount any mirrored ISOs for #{mirror_name}" do
-      path "['/usr/bin', '/bin', '/usr/sbin', '/sbin']"
+      path ['/usr/bin', '/bin', '/usr/sbin', '/sbin']
       command "umount #{www_dir}/disc*"
       user "root"
       group "root"
