a4477ac6b0a8d0a66469d66b56f9f60633f9fbf1
Jeremy MAURO <j.mauro@criteo.com>
2014-06-09 19:42:56 +0200

    Update version

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'j.mauro@criteo.com'
 license          'All rights reserved'
 description      'Installs/Configures mrepo'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.0'
+version          '0.5.1'
 depends          'yum-epel'
